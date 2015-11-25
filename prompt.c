9059bef5d74b4fe628256e518fb836c42c10614f
Tim Blankers <timblankers@gmail.com>
2015-11-25 22:25:34 +0100

    Modified prompt sign

--- prompt.c
+++ prompt.c
@@ -27,13 +27,14 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv) {
 
-	printf("Lispy Version 0.0.0.0.1\n");
+	printf("Lispy Version 0.0.0.0.2\n");
+	printf("Author: Tim Blankers with a noisy rooster\n");
 	printf("Press Ctrl+c to Exit\n");
 
 	while (1) {
-		char* input = readline("lispy> ");
+		char* input = readline("toktok> ");
 		add_history(input);
-		printf("No you're a %s\n", input);
+		printf("No you're a %s TOOOK!\n", input);
 		free(input);
 	}
 	return 0;
