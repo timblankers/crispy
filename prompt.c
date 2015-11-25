a84b695b5b37a53ab612f78bb5dc42c16040692a
Tim Blankers <timblankers@gmail.com>
2015-11-25 22:14:56 +0100

    Added Windows platform-specific preprocessor code

--- prompt.c
+++ prompt.c
@@ -3,6 +3,22 @@
 
 /* Windows only functions */
 #ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Readline function replacement */
+char* readline(char* prompt) {
+	fputs(prompt, stdout);					// output prompt
+	fgets(buffer, 2048, stdin);				// receive command
+	char* cpy = malloc(strlen(buffer)+1);	// allocate space for copy of command
+	strcpy(cpy, buffer);					// copy command
+	cpy[strlen(cpy)-1] = '\0';				// add string terminator
+	return cpy;
+}
+
+/* add_history function replacement */
+void add_history(char* unused) {}
 
 /* If not Windows, include the editline headers */
 #else
