f9a5870931fb09836f303b8bbea195d41cd784b4
Tim Blankers <timblankers@gmail.com>
2015-11-25 14:30:16 +0100

    Playing with structs

--- loops.c
+++ loops.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#include <string.h>
+
+typedef struct Movies {
+    char title[50];
+    char director[50];
+    char imdb[50];
+    short stars;
+    short release_date;
+} Movie;
 
 void hello(int n) {
     printf("\nPrinting %d times:\n", n);
@@ -21,6 +30,32 @@ int main(int argc, char** argv) {
     }
 
     hello(3);
+
+    int y = 2;
+
+    switch(y) {
+        case 0:
+            printf("It's zero\n");
+            break;
+        case 1:
+            printf("It's one\n");
+            break;
+        case 2:
+            printf("It's two\n");
+            break;
+        default:
+            printf("I have no idea\n");
+    }
+
+    Movie sample;
+    strcpy(sample.title, "Star Wars");
+    strcpy(sample.director, "George Lucas");
+    strcpy(sample.imdb, "http://www.imdb.com/title/tt0076759/");
+    sample.stars = 4;
+    sample.release_date = 2510;
+
+    printf("Director: %s\n", sample.director);
+
 
     return 0;
 }
