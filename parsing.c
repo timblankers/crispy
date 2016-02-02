993659f5db1d85bf3bcaade3c3c9321e6167a55a
Tim Blankers <timblankers@gmail.com>
2016-02-02 15:59:06 +0100

    Added parser Polish notation

--- parsing.c
+++ parsing.c
@@ -28,15 +28,47 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv) {
 
-	printf("Lispy Version 0.0.0.0.3\n");
+	/* Create Some Parsers */
+	mpc_parser_t* Number   = mpc_new("number");
+	mpc_parser_t* Operator = mpc_new("operator");
+	mpc_parser_t* Expr     = mpc_new("expr");
+	mpc_parser_t* Crispy    = mpc_new("crispy");
+
+	/* Define them with the following Language */
+	mpca_lang(MPCA_LANG_DEFAULT,
+	  "                                                     \
+	    number   : /-?[0-9]+([.][0-9]+)?/ ;                             \
+	    operator : '+' | '-' | '*' | '/' | '%' | /add/ | /sub/ | /mul/ | /div/;    \
+	    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+	    crispy    : /^/ <operator> <expr>+ /$/ ;             \
+	  ",
+	  Number, Operator, Expr, Crispy);
+
+	printf("Crispy Version 0.0.0.0.3\n");
 	printf("Author: Tim Blankers with a noisy rooster\n");
 	printf("Press Ctrl+c to Exit\n");
 
 	while (1) {
 		char* input = readline("toktok> ");
 		add_history(input);
-		printf("No you're a %s TOOOK!\n", input);
+
+		/* Attempt to Parse the user Input */
+		mpc_result_t r;
+		if (mpc_parse("<stdin>", input, Crispy, &r)) {
+		  /* On Success Print the AST */
+		  mpc_ast_print(r.output);
+		  mpc_ast_delete(r.output);
+		} else {
+		  /* Otherwise Print the Error */
+		  mpc_err_print(r.error);
+		  mpc_err_delete(r.error);
+		}
+
 		free(input);
 	}
+
+	/* Undefine and Delete our Parsers */
+	mpc_cleanup(4, Number, Operator, Expr, Crispy);
+
 	return 0;
 }
